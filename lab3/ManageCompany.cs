9a481f865efc2d66549d5e828bb263f554d5b29d
SumJest <summedjesters@gmail.com>
2022-09-19 21:05:25 +0500

    add lab3

---
+++ lab3/ManageCompany.cs
@@ -0,0 +1,244 @@
+﻿using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace lab3
+{
+    /// <summary>
+    /// Тип квартиры
+    /// </summary>
+    public enum FlatType
+    {
+        OneRoomed,
+        TwoRoomed,
+        ThreeRoomed,
+        FourRoomed
+    }
+    /// <summary>
+    /// Тип комнаты
+    /// </summary>
+    public enum BuildingType
+    {
+        House,
+        NonResidentialPremises
+    }
+    /// <summary>
+    /// Здание
+    /// </summary>
+    [XmlInclude(typeof(House))]
+    [XmlInclude(typeof(NonResidentialPremises))]
+    public abstract class Building
+    {
+        /// <summary>
+        /// Количество людей
+        /// </summary>
+        public double HumansCount { set; get; }
+        
+        /// <summary>
+        /// Адресс
+        /// </summary>
+        public string Address { set; get; }
+        
+        public abstract BuildingType Type { get; }
+
+        public abstract void ToCountHumans();
+
+        public void Print()
+        {
+            Console.WriteLine($"{Type.ToString()} / {Address} / {HumansCount}");
+        }
+
+    }
+    /// <summary>
+    /// Жилое здание
+    /// </summary>
+    public class House : Building
+    {
+
+        public readonly BuildingType _type = BuildingType.House;
+        
+        public override BuildingType Type
+        {
+            get { return _type; }
+        }
+        /// <summary>
+        /// Квартиры
+        /// </summary>
+        public List<FlatType> Flats { set; get; }
+        
+        public override void ToCountHumans()
+        {
+
+            int RoomsCount = 0;
+            foreach (var flat in Flats)
+            {
+                RoomsCount += (int)flat + 1;
+            }
+
+            HumansCount = Math.Round(RoomsCount * 1.3);
+        }
+        
+        public House(string address, List<FlatType> flats)
+        {
+            Flats = flats;
+            ToCountHumans();
+            Address = address;
+        }
+
+        public House()
+        {
+            Flats = new List<FlatType>();
+        }
+        
+
+
+
+
+    }
+    /// <summary>
+    /// Нежилое здание
+    /// </summary>
+    public class NonResidentialPremises : Building
+    {
+        
+        private readonly BuildingType _type = BuildingType.NonResidentialPremises;
+        
+        public override BuildingType Type
+        {
+            get { return _type; }
+        }
+        
+        /// <summary>
+        /// Площадь
+        /// </summary>
+        public float Area { set; get; }
+
+        public NonResidentialPremises()
+        {
+            Area = 0f;
+        }
+        
+        public NonResidentialPremises(string address, float area)
+        {
+            Area = area;
+            ToCountHumans();
+            Address = address;
+        }
+
+        public override void ToCountHumans()
+        {
+            HumansCount = Math.Round(Area * 0.2);
+        }
+    }
+
+    public class ManagementCompany
+    {
+        private List<Building> _buildings;
+
+        public double HumansCount;
+        
+        public ManagementCompany()
+        {
+            _buildings = new List<Building>();
+            HumansCount = 0;
+        }
+
+        public ManagementCompany(List<Building> buildings)
+        {
+            _buildings = buildings;
+            foreach (var building in _buildings)
+            {
+                HumansCount += building.HumansCount;
+            }
+        }
+
+        public void Add(Building building)
+        {
+            _buildings.Add(building);
+            HumansCount += building.HumansCount;
+        }
+        public void AddRange(List<Building> buildings)
+        {
+            _buildings.AddRange(buildings);
+            HumansCount += buildings.Sum(x => x.HumansCount);
+        }
+        
+        public void Print()
+        {
+            foreach (var building in _buildings)
+            {
+                building.Print();
+            }
+        }
+
+        public void PrintFirtsThreeBuildings()
+        {
+            if (_buildings.Count < 3)
+            {
+                Console.WriteLine("Not enough objects");
+                return;
+            }
+            for (var i = 0; i < 3; i++)
+            {
+                _buildings[i].Print();
+            }
+        }
+
+        public void PrintLastFourAddresses()
+        {
+            if (_buildings.Count < 4)
+            {
+                Console.WriteLine("Not enough objects");
+                return;
+            }
+
+            for (var i = _buildings.Count - 4; i < _buildings.Count; i++)
+            {
+                _buildings[i].Print();
+            }
+        }
+        
+        public void Sort()
+        {
+            _buildings = _buildings.OrderBy(a => a.HumansCount)
+                .ThenBy(a => (int) a.Type)
+                .ThenBy(a => a.Address)
+                .ToList();
+        }
+
+        public void toXml(string filename)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Building>));
+            
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                
+                XmlWriterSettings settings = new XmlWriterSettings();
+                settings.Indent = true;
+                settings.IndentChars = "\t";
+                XmlWriter writer = XmlWriter.Create(fs, settings);
+                serializer.Serialize(writer, _buildings);
+            
+                Console.WriteLine("Successful");
+                writer.Close();
+            }
+        }
+
+        public static ManagementCompany fromXml(string filename)
+        {
+            ManagementCompany company = new ManagementCompany();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Building>));
+
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            {
+                List<Building> buildings = serializer.Deserialize(fs) as List<Building>;
+                if (buildings != null) company.AddRange(buildings);
+                
+            }
+
+            return company;
+        }
+
+    }
+}
--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,178 +1,106 @@
-﻿namespace LabNamespace
+﻿using System.Runtime.InteropServices.ComTypes;
+
+namespace lab3
 {
-    internal class Project
+    public class Project
     {
 
-        public enum FlatType
-        {
-            OneRoomed,
-            TwoRoomed,
-            ThreeRoomed,
-            FourRoomed
-        }
-
-        public enum BuildingType
-        {
-            House,
-            NonResidentialPremises
-        }
-
-        public abstract class Building
-        {
-            public double HumansCount;
-
-            public string Address;
-            
-            public abstract BuildingType Type { get; }
-
-            public abstract void ToCountHumans();
-
-            public void Print()
-            {
-                Console.WriteLine($"{Type.ToString()} / {Address} / {HumansCount}");
-            }
-
-        }
-
-        class House : Building
-        {
-
-            private readonly BuildingType _type = BuildingType.House;
-            
-            public override BuildingType Type
-            {
-                get { return _type; }
-            }
-            
-            private List<FlatType> _flats;
-            
-            public override void ToCountHumans()
-            {
-
-                int RoomsCount = 0;
-                foreach (var flat in _flats)
-                {
-                    RoomsCount += (int)flat + 1;
-                }
-
-                HumansCount = RoomsCount * 1.3;
-            }
-            
-            public House(string address, List<FlatType> flats)
-            {
-                _flats = flats;
-                ToCountHumans();
-                Address = address;
-            }
-            
-
-
-
-
-        }
-
-        class NonResidentialPremises : Building
-        {
-            
-            private readonly BuildingType _type = BuildingType.NonResidentialPremises;
-            
-            public override BuildingType Type
-            {
-                get { return _type; }
-            }
-            
-            private float _area;
-            
-            public NonResidentialPremises(string address, float area)
-            {
-                _area = area;
-                ToCountHumans();
-                Address = address;
-            }
-
-            public override void ToCountHumans()
-            {
-                HumansCount = _area * 0.2;
-            }
-        }
-
-        public class ManagementCompany
-        {
-            private List<Building> _buildings;
-
-            public double HumansCount;
-            
-            public ManagementCompany()
-            {
-                _buildings = new List<Building>();
-                HumansCount = 0;
-            }
-
-            public ManagementCompany(List<Building> buildings)
-            {
-                _buildings = buildings;
-                _sort();
-                foreach (var building in _buildings)
-                {
-                    HumansCount += building.HumansCount;
-                }
-            }
-
-            public void Add(Building building)
-            {
-                _buildings.Add(building);
-                HumansCount += building.HumansCount;
-                _sort();
-            }
-            
-            public void Print()
-            {
-                foreach (var building in _buildings)
-                {
-                    building.Print();
-                }
-            }
-
-            public void PrintFirtsThreeBuildings()
-            {
-                if (_buildings.Count < 3)
-                {
-                    Console.WriteLine("Not enough objects");
-                    return;
-                }
-                for (var i = 0; i < 3; i++)
-                {
-                    _buildings[i].Print();
-                }
-            }
-
-            public void PrintLastFourAddresses()
-            {
-                if (_buildings.Count < 4)
-                {
-                    Console.WriteLine("Not enough objects");
-                    return;
-                }
-
-                for (var i = _buildings.Count - 4; i < _buildings.Count; i++)
-                {
-                    _buildings[i].Print();
-                }
-            }
-            
-            private void _sort()
-            {
-                _buildings = _buildings.OrderBy(a => a.HumansCount)
-                    .ThenBy(a => (int) a.Type)
-                    .ThenBy(a => a.Address)
-                    .ToList();
-            }
-
-        }
 
         private static void Main(string[] args)
         {
-            
+
+            ManagementCompany company;
+            Console.WriteLine("Please write file name which contains company xml structure (relative path with .xml extension)");
+            Console.WriteLine("Or leave path empty to use example");
+            Console.WriteLine($"Current path: {Directory.GetCurrentDirectory()}\\");
+            Console.Write("Filename: ");
+            string filename = Console.ReadLine();
+            if (filename == String.Empty)
+            {
+                company = new ManagementCompany();
+                company.Add(new House("Chelyabinsk ul. Smart, d. 178", new List<FlatType>() {FlatType.FourRoomed, FlatType.TwoRoomed, FlatType.OneRoomed}));
+                company.Add(new House("Chelyabinsk ul. Smart, d. 95", new List<FlatType>() {FlatType.ThreeRoomed, FlatType.TwoRoomed, FlatType.ThreeRoomed}));
+                company.Add(new House("Chelyabinsk ul. Smart, d. 132", new List<FlatType>() {FlatType.TwoRoomed, FlatType.OneRoomed, FlatType.FourRoomed}));
+                company.Add(new NonResidentialPremises("Chelyabinsk ul. Lenina, d. 37", 232.6F));
+                company.Add(new NonResidentialPremises("Chelyabinsk ul. Smart, d. 178", 45F));
+                company.Add(new House("Chelyabinsk ul. Pushkina, d. 95", new List<FlatType>()
+                {
+                    FlatType.ThreeRoomed, FlatType.TwoRoomed, FlatType.ThreeRoomed,
+                    FlatType.FourRoomed, FlatType.FourRoomed, FlatType.ThreeRoomed,
+                    FlatType.FourRoomed, FlatType.ThreeRoomed, FlatType.TwoRoomed
+                }));
+            }
+            else
+            {
+                try
+                {
+                    company = ManagementCompany.fromXml(filename);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Wrong filename or file content");
+                    return;
+                }
+            }
+
+            while (true)
+            {
+                // Menu
+                Console.WriteLine("\n\n1. Sort\n2. Print all building\n3. Print first 3 buildings\n4. Print last 4 buildings" +
+                                  "\n5. Print humans count\n6. Save to file (.xml)\n0. Exit\n\n");
+                
+                Console.Write("Choose: ");
+                string input = Console.ReadLine();
+                int choice = 0;
+                bool is_int = int.TryParse(input, out choice);
+                if (!is_int)
+                {
+                    Console.WriteLine("Choose right number!");
+                    continue;
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        company.Sort();
+                        break;
+                    case 2:
+                        company.Print();
+                        break;
+                    case 3:
+                        company.PrintFirtsThreeBuildings();
+                        break;
+                    case 4:
+                        company.PrintLastFourAddresses();
+                        break;
+                    case 5:
+                        Console.WriteLine($"\nHumans count: {company.HumansCount}");
+                        break;
+                    case 6:
+                        Console.WriteLine("Please write file name to save company xml structure (relative path with .xml extension)");
+                        Console.WriteLine($"Current path: {Directory.GetCurrentDirectory()}\\");
+                        Console.Write("Filename: ");
+                        string savefilename = Console.ReadLine();
+                        try
+                        {
+                            company.toXml(savefilename);
+                            Console.WriteLine($"Company successfully saved to {savefilename}.");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine($"Error to save to {savefilename}");
+                        }
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        Console.WriteLine("Choose right number!");
+                        break;
+                        
+                }
+            }
         }
+            
+            
     }
 }
