f25d9c9658970d616ada75da11a36aef57f9a9bf
SumJest <summedjesters@gmail.com>
2022-09-22 22:39:32 +0500

    lab4 finish

---
+++ lab4/CSVWriter/CSVWriter.cs
@@ -0,0 +1,26 @@
+﻿namespace lab4;
+
+
+
+public class CSVWriter
+{
+
+    private readonly string _path;
+
+    public CSVWriter(string path)
+    {
+        _path = path;
+    }
+
+    public void Write(Phone[] phones)
+    {
+        string data = "";
+
+        foreach (var phone in phones)
+        {
+            data += $"{phone.Number},{phone.UrlName},{phone.Url},{phone.Level}\n";
+        }
+        
+        File.WriteAllText(_path, data);
+    }
+}
--- lab4/Program.cs
+++ lab4/Program.cs
@@ -1,8 +1,51 @@
-﻿namespace LabNamespace
+﻿
+
+namespace lab4
 {
     internal class Project
     {
+
+        private static List<Phone> _phones = new List<Phone>();
         
+        public static void LinkEvent(string url, string name, int level)
+        {
+            //Console.WriteLine($"{level} - {url} - {name}");
+        }
+
+        public static void PhoneEvent(Phone phone)
+        {
+            Console.WriteLine($"Phone found: {phone.Number}. In page by link: {phone.Url}");
+            _phones.Add(phone);
+        }
+        
+        private static void Main(string[] args)
+        {
+            Console.Write("Enter url (with last \"/\"): ");
+            string url = Console.ReadLine();
+            Console.Write("Enter the nesting: ");
+            int nesting = 2;
+            if (!int.TryParse(Console.ReadLine(), out nesting))
+            {
+                Console.WriteLine($"Enter NUMBER! Nesting is {nesting}");
+            }
+
+            int page_count = 10;
+            Console.Write("Enter max page value: ");
+            if (!int.TryParse(Console.ReadLine(), out page_count))
+            {
+                Console.WriteLine($"Enter NUMBER! Max page count is {page_count}");
+            }
+            
+            Handler handler = new Handler(url, nesting, page_count);
+            handler.UrlFoundEvent += LinkEvent;
+            handler.PhoneFoundEvent += PhoneEvent;
+            CSVWriter writer = new CSVWriter("phones.csv");
+            handler.Start();
+            if (_phones.Count > 0)
+            {
+                writer.Write(_phones.ToArray());
+            }
+        }
     }
 }
 
